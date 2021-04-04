1a5a6f732daa6e505ac19d709f84f05fb20cc21e
Svitlovskyi <maksym.svitlovskyi@gmail.com>
2021-04-04 19:17:51 +0200

    add monte carlo

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 import value_iteration as vi
+import policy_iteration as pi
 import gym
 
 env = gym.make('FrozenLake-v0')
-gamma = 0.99
-theta = 0.000001
+gamma = 0.9
+theta = 0.001
 
-value_iter = vi.ValueIteration(env)
-value_iter.value_iteration(gamma, theta)
-value_iter.simulate_value_iteration(100)
+
+policy_iter = pi.PolicyIteration(env)
+policy_iter.policy_iteration(gamma, theta)
+
+# env.reset()
+# env.render()
+
+# value_iter = vi.ValueIteration(env)
+# value_iter.value_iteration(gamma, theta)
+# value_iter.simulate_value_iteration(10000)
+
