a02c3e41628e731950f8285802a170e595a8cab0
Svitlovskyi <maksym.svitlovskyi@gmail.com>
2021-04-04 22:08:25 +0200

    add comments

--- value_iteration.py
+++ value_iteration.py
@@ -1,8 +1,8 @@
 import gym
 import numpy as np
 
+
 class ValueIteration:
-    
     def __init__(self, env):
         self.env = env
         self.N_STATES = env.env.nS
@@ -11,15 +11,23 @@ class ValueIteration:
         self._final_policy = None
 
     def value_iteration(self, gamma, theta):
-        V = np.zeros(self.N_STATES)
+        """
+
+        :param gamma: discount factor
+        :param theta: threshold
+        :return: optimal policy
+        """
+
+        V = np.zeros(self.N_STATES)  # empty array with value functions
 
         while True:
             delta = 0
 
             for state in range(self.N_STATES):
                 prev_value = V[state]
+
                 V[state] = self._bellman_optimization_equality(state, V, gamma)
-                delta = max(delta, abs(prev_value - V[state]))
+                delta = max(delta, abs(prev_value - V[state]))  # difference between value function of prev and current
 
             if delta < theta:
                 break
@@ -29,8 +37,55 @@ class ValueIteration:
         self._final_policy = final_policy
         return final_policy
 
+    def _bellman_optimization_equality(self, state, V, gamma):
+        """
 
-    def simulate_value_iteration(self, episodes):
+        :param state:
+        :param V: array of value functions
+        :param gamma: disocunt factor
+        :return: best action
+        """
+
+        action_values = list()
+
+        for action in range(self.N_ACTIONS):
+            trans_prob = self.trans_probs[state][action]
+            action_value = 0
+
+            for i in trans_prob:
+                s_ = i[1]
+                p = i[0]
+                r = i[2]
+                action_value += p * (r + gamma * V[s_])
+            action_values.append(action_value)
+
+        return max(action_values)
+
+    def _generate_final_policy(self, V, gamma):
+        """
+
+        :param V: array of value functions
+        :param gamma: discount factor
+        :return: optimal policy
+        """
+        policy = np.zeros(self.N_STATES)
+        for state in range(self.N_STATES):
+            action_values = list()
+            for action in range(self.N_ACTIONS):
+                trans_prob = self.trans_probs[state][action]
+                action_value = 0
+
+                for i in trans_prob:
+                    s_ = i[1]
+                    p = i[0]
+                    r = i[2]
+                    action_value += p * (r + gamma * V[s_])
+                action_values.append(action_value)
+            best_action = np.argmax(action_values)
+            policy[state] = best_action
+        return policy
+
+    def _simulate_value_iteration(self, episodes):
         if self._final_policy is None:
             raise Exception("You need to train first")
         else:
@@ -49,36 +104,3 @@ class ValueIteration:
                         break
 
             print(" agent succeeded to reach goal {} out of {} Episodes using this policy ".format(wins, episodes))
-
-    def _bellman_optimization_equality(self, state, V, gamma):
-        action_values = list()
-        for action in range(self.N_ACTIONS):
-            trans_prob = self.trans_probs[state][action]
-            action_value = 0
-
-            for i in trans_prob:
-                s_ = i[1]
-                p = i[0]
-                r = i[2]
-                action_value += p * (r + gamma * V[s_])
-            action_values.append(action_value)
-
-        return max(action_values)
-
-    def _generate_final_policy(self, V, gamma):
-        policy = np.zeros(self.N_STATES)
-        for state in range(self.N_STATES):
-            action_values = list()
-            for action in range(self.N_ACTIONS):
-                trans_prob = self.trans_probs[state][action]
-                action_value = 0
-
-                for i in trans_prob:
-                    s_ = i[1]
-                    p = i[0]
-                    r = i[2]
-                    action_value += p * (r + gamma * V[s_])
-                action_values.append(action_value)
-            best_action = np.argmax(action_values)
-            policy[state] = best_action
-        return policy
