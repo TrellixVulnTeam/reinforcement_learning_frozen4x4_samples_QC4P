a297bbf8b9381bafa6a78b9fb6400c935ab55dd9
Svitlovskyi <maksym.svitlovskyi@gmail.com>
2021-04-10 17:32:05 +0200

    add comments

--- Utils.py
+++ Utils.py
@@ -8,7 +8,15 @@ Utils for frozen lake
 
 class Utils:
     @staticmethod
+
     def test_game(env, policy, sleep=False):
+        """
+        start one game with visualization
+        :param env: gym env
+        :param policy: policy in dict format {state: best action}
+        :param sleep: turn on/off visualization
+        :return: game reward
+        """
         state = env.reset()
         if sleep:
             env.render()
@@ -29,6 +37,12 @@ class Utils:
 
     @staticmethod
     def test_on_eps(env, policy, episodes):
+        """
+        test algorithm on count of eps, and print results
+        :param env: gym env
+        :param policy: policy in dict format {state: best action}
+        :param episodes: the number of episodes on which the algorithm is testing
+        """
         wins = 0
 
         for _ in range(episodes):
@@ -41,6 +55,11 @@ class Utils:
 
     @staticmethod
     def create_csv(policy, name):
+        """
+        create csv with policy
+        :param policy: policy in dict format {state: best action}
+        :param name: name of file
+        """
         with open(name, 'w', newline='') as file:
             writer = csv.writer(file)
             for key in policy.keys():
@@ -50,6 +69,11 @@ class Utils:
 
     @staticmethod
     def read(name):
+        """
+
+        :param name: name of file
+        :return: policy in dict format {state: best action}
+        """
         policy_dict = {}
         with open(name) as file:
             csv_reader = csv.reader(file, delimiter=',')
