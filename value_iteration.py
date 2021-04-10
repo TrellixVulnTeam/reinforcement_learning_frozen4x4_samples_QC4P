f24e1873db10a355ce907224fc0306bac164a320
Svitlovskyi <maksym.svitlovskyi@gmail.com>
2021-04-10 14:54:30 +0200

    add some comments, clean code

--- value_iteration.py
+++ value_iteration.py
@@ -1,9 +1,12 @@
-import gym
 import numpy as np
 
 
 class ValueIteration:
     def __init__(self, env):
+        """
+
+        :param env: gym environment
+        """
         self.env = env
         self.N_STATES = env.env.nS
         self.N_ACTIONS = env.env.nA
@@ -12,10 +15,10 @@ class ValueIteration:
 
     def value_iteration(self, gamma, theta):
         """
-
+        value iteration train
         :param gamma: discount factor
         :param theta: threshold
-        :return: optimal policy
+        :return: optimal policy (dict where key is state, value is action)
         """
 
         V = np.zeros(self.N_STATES)  # empty array with value functions
@@ -37,12 +40,15 @@ class ValueIteration:
         self._final_policy = final_policy
         return final_policy
 
+    '''
+    private methods
+    '''
+
     def _bellman_optimization_equality(self, state, V, gamma):
         """
-
         :param state:
         :param V: array of value functions
-        :param gamma: disocunt factor
+        :param gamma: discount factor
         :return: best action
         """
 
@@ -68,7 +74,7 @@ class ValueIteration:
         :param gamma: discount factor
         :return: optimal policy
         """
-        policy = np.zeros(self.N_STATES)
+        policy = {}
         for state in range(self.N_STATES):
             action_values = list()
             for action in range(self.N_ACTIONS):
@@ -84,23 +90,3 @@ class ValueIteration:
             best_action = np.argmax(action_values)
             policy[state] = best_action
         return policy
-
-    def _simulate_value_iteration(self, episodes):
-        if self._final_policy is None:
-            raise Exception("You need to train first")
-        else:
-            wins = 0
-            for ep in range(1, episodes + 1):
-                obs = self.env.reset()
-                print("episode: {}".format(ep))
-
-                for turn in range(100):
-                    obs, reward, done, info = self.env.step(int(self._final_policy[obs]))
-
-                    if done:
-                        if reward == 1:
-                            wins += 1
-                        self.env.render()
-                        break
-
-            print(" agent succeeded to reach goal {} out of {} Episodes using this policy ".format(wins, episodes))
